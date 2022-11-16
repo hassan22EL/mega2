42c48456304a24a04023a5ac7a0e8f459ea85d2e
hassan22EL <https://github.com/hassan22EL>
2022-11-17 00:07:34 +0200

    modifiy some commit

--- src/mega/gpio/gpio.c
+++ src/mega/gpio/gpio.c
@@ -1,35 +1,38 @@
-
-/* 
- * *****************************************************************************
- *                        Module  Definition                                   *
- * *****************************************************************************
- * File         :   gpio.h
- * Author       :   Hassan Elsaied
- * Data Memory  :   no data byte used
- * Program Space:   under upgrade
- * Version      :   Mega2v241022
- * Start Data   :   24-10-2022  20:00:00
- * End Data     :   24-10-2022  23:48:00
- * Work Time    :   5-hour
- * Comments     :   no comment
+/*
+--------------------------------------------------------------------------------------------------
+|                           < Module  Definition >                                               | 
+--------------------------------------------------------------------------------------------------
+| < FILE                     : gpio.c                                                             |                                  
+| < Author                   : Hassan Elsaied                                                    |
+| < Version                  : Mega2v241022                                                      |
+| < Refences                 : no refences used                                                  |
+| < SRAM USAGE               : no- byte used                                                     |
+| < PROGRAM USAGE            : depent in application                                             |                                      
+| < Hardware Usage           : GPIO                                                              |
+| < File Created             : 24-10-2022                                                        |
+--------------------------------------------------------------------------------------------------
  */
-
 #include "../../../inc/mega.h"
 
 /* 
- * ******************************************************************************
- *                        Call Functions                                        *
- * ******************************************************************************
+ --------------------------------------------------------------------------------------------------
+ |                        < Call Functions >                                                     |
+ --------------------------------------------------------------------------------------------------
  */
 
-/* 
- * ******************************************************************************
- *                        internal_pinMode2                                        *
- * ******************************************************************************
- * benfit       : select mode of the pin (output or input or input with enable pull up
- * @param pin   : pin address of gpio
- * @param mode  : type of the mode select from mode number (MODE_INPUT or MODE_INPUT_PULLUP or MODE_OUTPUT)
- * @return      : void
+/*
+ --------------------------------------------------------------------------------------------------
+ |                                 < _digitalpinMode >                                            |
+ --------------------------------------------------------------------------------------------------
+ | < @Function          : void _digitalpinMode                                                    |  
+ | < @Description       : select mode of the pin (output or input or input with enable pull up    | 
+ | < @Param pin         : pin address of gpio  (GPIO_Px) x form 0 to 7 , P (A ,B,C,....)          |   
+ | < @Param mode        : type of the mode select one mode form 3 mode                            |
+ |                      : @MODE_INPUT set pin as input                                            |
+ |                      : @MODE_OUTPUT set pin as output                                          |
+ |                      : @MODE_INPUT_PULLUP set pin as input and enable pull up                  |
+ | < @return            : void                                                                    |
+ --------------------------------------------------------------------------------------------------
  */
 
 void _digitalpinMode(gpio_t pin, uint8_t mode) {
@@ -53,15 +56,16 @@ void _digitalpinMode(gpio_t pin, uint8_t mode) {
     }
 }
 
-/* 
- * ******************************************************************************
- *                        internal_digitalRead2                                        *
- * ******************************************************************************
- * benfit       : get pin is high or low just return output number GPIO_LOW is pin is low and GPIO_HIGH pin is high
- * @param pin   : pin address of gpio
- * @return      : 0 or 1
+/*
+ --------------------------------------------------------------------------------------------------
+ |                                 < _digitalPinRead >                                            |
+ --------------------------------------------------------------------------------------------------
+ | < @Function          : uint8_t _digitalPinRead                                                 |  
+ | < @Description       : get pin state is high or low just                                       |
+ | < @Param pin         : pin address of gpio  (GPIO_Px) x form 0 to 7 , P (A ,B,C,....)          |                    
+ | < @return            : utput number GPIO_LOW is pin is low and GPIO_HIGH                       |
+ --------------------------------------------------------------------------------------------------
  */
-
 uint8_t _digitalPinRead(gpio_t pin) {
     if ((GPIO_PIN_REG(pin) & GPIO_PIN_MASK(pin)) != 0)
         return GPIO_HIGH;
@@ -79,6 +83,22 @@ uint8_t _digitalPinRead(gpio_t pin) {
  * @param value : type of out number (GPIO_LOW,GPIO_HIGH,GPIO_TGL)
  * @return      : void
  */
+
+/*
+ --------------------------------------------------------------------------------------------------
+ |                                 < _digitalPinWrite >                                           |
+ --------------------------------------------------------------------------------------------------
+ | < @Function          : void _digitalPinWrite                                                   |  
+ | < @Description       : write out of the pin (low or high or toggle)                            | 
+ | < @Param pin         : pin address of gpio  (GPIO_Px) x form 0 to 7 , P (A ,B,C,....)          |   
+ | < @Param mode        : type of the mode select one mode form 3 mode                            |
+ |                      : @GPIO_LOW change pin state to low                                       |
+ |                      : @GPIO_HIGH change pin state to high                                     |
+ |                      : @GPIO_TGL  change current pin state form high to low or low to high     |
+ | < @return            : void                                                                    |
+ --------------------------------------------------------------------------------------------------
+ */
+
 void _digitalPinWrite(gpio_t pin, uint8_t value) {
     ATOMIC_BEGIN
     if (value == GPIO_HIGH) {
